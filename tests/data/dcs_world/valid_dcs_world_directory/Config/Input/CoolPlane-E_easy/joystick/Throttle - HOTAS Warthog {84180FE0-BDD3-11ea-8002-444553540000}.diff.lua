c7bd75b9e6b6ebf6181d762ad4276b73133c89a4
Robert Cox <r.cox@live.co.uk>
2020-09-24 11:21:36 +0100

    File fixes

---
+++ tests/data/dcs_world/valid_dcs_world_directory/Config/Input/CoolPlane-E_easy/joystick/Throttle - HOTAS Warthog {84180FE0-BDD3-11ea-8002-444553540000}.diff.lua
@@ -0,0 +1,149 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2012cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+			["name"] = "Zoom View",
+		},
+		["a2087cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Z",
+				},
+			},
+			["name"] = "Flight Control Collective",
+		},
+		["a3026cd3"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+			["name"] = "Throttle",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d3001pnilu3001cd52vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "External Cargo Pilot Unhook",
+		},
+		["d3005pnilunilcd49vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+			["name"] = "Pilot Sight Armed/Docked",
+		},
+		["d3007pnilunilcd49vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "Pilot Sight Switch",
+		},
+		["d3012pnilunilcd21vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "Intercom Mode Selector (rotary)",
+		},
+		["d3018pnilu3018cd3vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+			["name"] = "Start-up engine",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+		},
+		["d3037pnilunilcd7vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Search light Off",
+		},
+		["d3037pnilunilcd7vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Search light On",
+		},
+		["d511pnilu515cdnilvdnilvpnilvunil"] = {
+			["name"] = "Search light Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["d512pnilu515cdnilvdnilvpnilvunil"] = {
+			["name"] = "Search light Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["d513pnilu515cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Search light Retract",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["d514pnilu515cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Search light Extend",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+	},
+}
+return diff
