64f71995982928b055abaffd2c502f157952a194
Robert Cox <r.cox@live.co.uk>
2022-03-27 11:46:41 +0100

    POC interface with new input library, handling of DCS modifiers

---
+++ src/joystick_diagrams/adaptors/dcs_world_v2.py
@@ -0,0 +1,297 @@
+"""DCS World Lua Config Parser for use with Joystick Diagrams"""
+import os
+import re
+from pathlib import Path
+import logging
+from ply import lex, yacc
+import joystick_diagrams.adaptors.dcs_world_lex  # pylint: disable=unused-import
+import joystick_diagrams.adaptors.dcs_world_parse  # pylint: disable=unused-import
+import joystick_diagrams.adaptors.joystick_diagram_interface as jdi
+import joystick_diagrams.classes.input as ipt
+from typing import Optional
+
+_logger = logging.getLogger(__name__)
+
+EASY_MODE = "_easy"
+
+
+class DCSWorldParser(jdi.JDinterface):
+    def __init__(self, path, easy_modes=True):
+        jdi.JDinterface.__init__(self)
+        self.path = Path(path)
+        self.remove_easy_modes = easy_modes
+        self.base_directory = self.__validate_base_directory()
+        self.valid_profiles = self.__validate_profiles()
+        self.joystick_listing = {}
+        self.file = None
+        self.profiles_to_process = None
+        self.profile_devices = None
+        self.fq_path = None
+
+    def __validate_base_directory(self):
+        """validate the base directory structure, make sure there are files."""
+        if "Config" in os.listdir(self.path):
+            try:
+                return os.listdir(os.path.join(self.path, "Config", "Input"))
+            except FileNotFoundError:
+                raise FileNotFoundError("DCS: No input directory found") from None
+        else:
+            raise FileNotFoundError("DCS: No Config Folder found in DCS Folder.")
+
+    def __validate_profiles(self):
+        """
+        Validate Profiles Routine
+        """
+        if len(self.base_directory) > 0:
+            valid_items = []
+            for item in self.base_directory:
+                valid = self.__validate_profile(item)
+                if valid:
+                    valid_items.append(item)
+                else:
+                    _logger.info("DCS: Profile {} has no joystick directory files".format(item))
+        else:
+            raise FileExistsError("DCS: No profiles exist in Input directory!")
+
+        return valid_items
+
+    def __validate_profile(self, item):
+        """
+        Validate Inidividual Profile
+        Return Valid Profile
+        """
+
+        if os.path.isdir(os.path.join(self.path, "Config", "Input", item)) and "joystick" in os.listdir(
+            os.path.join(self.path, "Config", "Input", item)
+        ):
+            return os.listdir(os.path.join(self.path, "Config", "Input", item, "joystick"))
+
+        return False
+
+    def get_validated_profiles(self) -> list[str]:
+        """Expose Valid Profiles only to UI"""
+        if self.remove_easy_modes:
+            return list(
+                filter(
+                    lambda x: False if EASY_MODE in x else True,
+                    self.valid_profiles,
+                )
+            )
+        return self.valid_profiles
+
+    def convert_button_format(self, button) -> str:
+        """Convert DCS Buttons to match expected "BUTTON_X" format"""
+        split = button.split("_")
+
+        if len(split) == 2:
+            if split[1][0:3] == "BTN":
+                return split[1].replace("BTN", "BUTTON_")
+            elif split[1].isalpha():
+                return "AXIS_{}".format(split[1])
+            elif split[1][0:6] == "SLIDER":
+                return "AXIS_SLIDER_{}".format(split[1][6:])
+            else:
+                return split[1]
+
+        elif len(split) == 4:
+            return "{button}_{pov}_{dir}".format(button=split[1].replace("BTN", "POV"), pov=split[2][3], dir=split[3])
+
+    @staticmethod
+    def extract_device_details(file_name: str) -> tuple:
+        return (file_name[-46:-10], file_name[:-48])  # Guid  # Name
+
+    @staticmethod
+    def process_config_file(file_path: str) -> dict:
+        """
+        Process the config file
+        """
+        with open(file_path, "r") as file:
+            data = file.read()
+        return data
+
+    def process_profiles(self, profile_list: list) -> dict[str]:
+
+        if profile_list is None:
+            return "TODO - ERROR HANDLING"
+
+        final_dict = {
+            "axisDiffs": {},
+            "keyDiffs": {},
+        }
+
+        for profile in profile_list:
+            _logger.warning(f"Profile: {profile}")
+            fq_path = os.path.join(self.path, "Config", "Input", profile, "joystick")
+            profile_device_files = os.listdir(os.path.join(fq_path))
+
+            _logger.warning(f"Profile paths: {profile_device_files}")
+
+            for file in profile_device_files:
+
+                _logger.warning(f"Processing file: {file}")
+                guid, name = self.extract_device_details(file)
+
+                ipt.add_device(guid=guid, name=name)
+
+                if os.path.isdir(os.path.join(fq_path, file)):
+                    print("Skipping as Folder")
+                else:
+                    try:
+                        self.file = Path(os.path.join(fq_path, file)).read_text(encoding="utf-8")
+                        self.file = self.file.replace("local diff = ", "")  ## CLEAN UP
+                        self.file = self.file.replace("return diff", "")  ## CLEAN UP
+                    except FileNotFoundError:
+                        raise FileExistsError(
+                            "DCS: File {} no longer found - It has been moved/deleted from directory".format(file)
+                        )
+                    else:
+                        file_dictionary = self.parse_file()
+
+                        # Merge outputs into master dictionary in profile order
+                        if "axisDiffs" in file_dictionary.keys():
+                            final_dict["axisDiffs"] |= file_dictionary["axisDiffs"]
+
+                        if "keyDiffs" in file_dictionary.keys():
+                            final_dict["keyDiffs"] |= file_dictionary["keyDiffs"]
+
+        return final_dict
+
+    def create_joystick_map(self, data: dict, guid: str) -> dict:
+
+        if "keyDiffs" in data.keys():
+            for value in data["keyDiffs"].values():
+                print(f"Value is {value}")
+                for item, attribute in value.items():
+                    print(value.items())
+                    if item == "name":
+                        name = attribute
+                    if item == "added":
+                        button = self.convert_button_format(attribute[1]["key"])
+
+                    if button & name:
+                        ipt.add_inputs(guid, input_id=button, style=ipt.InputTypes.BUTTON, command=name, modifiers=[])
+
+        # if "axisDiffs" in data.keys():
+        #     for value in data["axisDiffs"].values():
+        #         for item, attribute in value.items():
+        #             if item == "name":
+        #                 name = attribute
+        #             if item in ["added", "changed"]:
+        #                 axis = self.convert_button_format(attribute[1]["key"])
+        #                 write_val = True
+        #         if write_val:
+        #             button_array.update({axis: name})
+        #             write_val = False
+
+    def parse_file(self) -> dict:
+        # pylint: disable=unused-variable
+        tokens = (
+            "LCURLY",
+            "RCURLY",
+            "STRING",
+            "NUMBER",
+            "LBRACE",
+            "RBRACE",
+            "COMMA",
+            "EQUALS",
+            "TRUE",
+            "FALSE",
+            "DOUBLE_VAL",
+        )
+
+        t_LCURLY = r"\{"  # pylint: disable=invalid-name
+        t_RCURLY = r"\}"  # pylint: disable=invalid-name
+        t_LBRACE = r"\["  # pylint: disable=invalid-name
+        t_RBRACE = r"\]"  # pylint: disable=invalid-name
+        t_COMMA = r"\,"  # pylint: disable=invalid-name
+        t_EQUALS = r"\="  # pylint: disable=invalid-name
+
+        def t_DOUBLE_VAL(t):  # pylint: disable=invalid-name
+            r"(\+|\-)?[0-9]+\.[0-9]+"
+            t.value = float(t.value)
+            return t
+
+        def t_NUMBER(t):  # pylint: disable=invalid-name
+            r"[0-9]+"
+            t.value = int(t.value)
+            return t
+
+        def t_STRING(t):  # pylint: disable=invalid-name
+            r"\"[\w|\/|\(|\)|\-|\:|\+|\,|\&|\.|\'|\<|\>|\s]+\" "
+            t.value = t.value[1:-1]
+            return t
+
+        def t_TRUE(t):  # pylint: disable=invalid-name
+            r"(true)"
+            t.value = True
+            return t
+
+        def t_FALSE(t):  # pylint: disable=invalid-name
+            r"(false)"
+            t.value = False
+            return t
+
+        t_ignore = " \t\n"
+
+        def t_error(t):  # pylint: disable=invalid-name
+            print("Illegal character '%s'" % t.value[0])
+            t.lexer.skip(1)
+
+        # Parsing rules
+
+        def p_dict(t):  # pylint: disable=invalid-name
+            """dict : LCURLY dvalues RCURLY"""
+            t[0] = t[2]
+
+        def p_dvalues(t):  # pylint: disable=invalid-name
+            """dvalues : dvalue
+            | dvalue COMMA
+            | dvalue COMMA dvalues"""
+            t[0] = t[1]
+            if len(t) == 4:
+                t[0].update(t[3])
+
+        def p_key_expression(t):  # pylint: disable=invalid-name
+            """key : LBRACE NUMBER RBRACE
+            | LBRACE STRING RBRACE"""
+            t[0] = t[2]
+
+        def p_value_expression(t):  # pylint: disable=invalid-name
+            """dvalue : key EQUALS STRING
+            | key EQUALS boolean
+            | key EQUALS DOUBLE_VAL
+            | key EQUALS NUMBER
+            | key EQUALS dict"""
+            t[0] = {t[1]: t[3]}
+
+        def p_boolean(p):  # pylint: disable=invalid-name
+            """boolean : TRUE
+            | FALSE
+            """
+            p[0] = p[1]
+
+        def p_error(t):  # pylint: disable=invalid-name
+            print("Syntax error at '%s'" % t.value)
+
+        # Build the lexer
+        lexer = lex.lex(
+            debug=False,
+            optimize=0,
+            # lextab="dcs_world_lex",
+            reflags=re.UNICODE | re.VERBOSE,
+        )
+
+        # Build the parser
+        parser = yacc.yacc(
+            debug=False,
+            optimize=0,
+            # tabmodule="dcs_world_parse"
+        )
+
+        # Parse the data
+        try:
+            data = parser.parse(self.file)
+        except Exception as error:
+            _logger.error(error)
+            raise
+        return data
--- src/joystick_diagrams/classes/input.py
+++ src/joystick_diagrams/classes/input.py
@@ -11,9 +11,11 @@ This module operates on FIFO basis, and is designed to be used by a single threa
 from dataclasses import dataclass
 from enum import auto, Enum
 import logging
+from typing import final
 
 _logger = logging.getLogger(__name__)
 _devices = {}
+
 
 @dataclass
 class Command:
@@ -49,6 +51,9 @@ class Input:
         Adds a modifier to an existing input, or amends an existing modifier
         """
         existing = self._check_existing_modifier(modifier)
+
+        _logger.warning(f"Existing modifier check is {existing}")
+
         if existing is None:
             _logger.warning(f"Modifier {modifier} for input {self.identifier} not found so adding")
             self.modifiers.append(Modifier(modifier, command))
@@ -57,18 +62,20 @@ class Input:
             existing.command = command
 
     def _check_existing_modifier(self, modifier: set) -> Modifier | None:
+
+        _logger.warning(f"Checking for existing modifier {modifier}")
+        _logger.warning(f"Existing modifiers: {self.modifiers}")
+
         for x in self.modifiers:
             if x.input == modifier:
                 return x
         return None
 
-    def __repr__(self):
-        return f"Input: {self.identifier} {self.style} {self.command} {self.modifiers}"
-
 
 class LogicalDevice:
-    def __init__(self, guid: str):
-        self.guid = guid
+    def __init__(self, guid: str, name: str):
+        self.guid = guid.strip().lower()
+        self.name = name.strip().lower()
         self.inputs = []
 
     def create_input(self, input_id: str, style: InputTypes, command: Command, modifiers: list[Modifier]) -> None:
@@ -88,15 +95,21 @@ class LogicalDevice:
                 return x
         return None
 
+    def get_device_inputs(self) -> dict:
+        return self.inputs
+
     def add_modifier_to_input(self, input_id, modifier: set, command: Command) -> None:
         """
         Adds a modifier to an input if it exists
         """
+
+        _logger.warning(f"Adding modifier {modifier} to input {input_id}")
         obj = self._get_input(input_id)
-        print(f"Object is: {obj}")
+        _logger.warning(f"Modifier input is: {obj}")
         if obj is None:
-            # Add error handling for scenario where input does not exist
-            return None
+            self.create_input(input_id, InputTypes.BUTTON, "Not Used", [])
+            obj = self._get_input(input_id)
+            _logger.warning(f"Modifier input created now: {obj}")
         obj.add_modifier(modifier, command)
 
     def __check_existing_input(self, input_id: str, style: InputTypes) -> Input | None:
@@ -109,13 +122,6 @@ class LogicalDevice:
             if x.identifier == input_id and x.style == style:
                 return x
         return None
-
-
-# What will consumers want to do?
-# 1. Add a device
-# - Not need to know or care that another class has instanciated it
-# 2. Add an input to a device
-# 3. Add a modifiers to a specific input
 
 
 def clear_devices():
@@ -134,11 +140,13 @@ def _get_device(guid: str) -> LogicalDevice:
     return _devices[guid]
 
 
-def add_device(guid: str) -> None:
+def add_device(guid: str, name: str) -> LogicalDevice:
     if guid not in _devices:
-        _devices.update({guid: LogicalDevice(guid)})
+        _devices.update({guid: LogicalDevice(guid, name)})
     else:
         _logger.warning(f"Device {guid} already exists and will not be re-added")
+
+    return _devices[guid]
 
 
 def add_input_modifier(guid: str, input_id: str, modifier: set, command: Command) -> None:
@@ -147,21 +155,3 @@ def add_input_modifier(guid: str, input_id: str, modifier: set, command: Command
 
 def add_inputs(guid: str, **kwargs) -> None:
     _devices[guid].create_input(**kwargs)
-
-
-add_device("92029209=09209202902")
-add_device("92029209=09209202902")
-
-add_inputs(
-    "92029209=09209202902", input_id="button_a", style=InputTypes.BUTTON, command=Command("button_a"), modifiers=[]
-)
-
-add_input_modifier("92029209=09209202902", "button_a", {"ctrl", "alt"}, Command("button_1"))
-
-add_inputs(
-    "92029209=09209202902", input_id="button_a", style=InputTypes.BUTTON, command=Command("button_G"), modifiers=[]
-)
-
-print("-" * 20)
-add_input_modifier("92029209=09209202902", "button_a", {"ctrl", "alt", "abc"}, Command("button_5"))
-add_input_modifier("92029209=09209202902", "button_a", {"ctrl", "alt", "del"}, Command("button_2"))
