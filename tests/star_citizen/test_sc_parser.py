3fc3599ee418a01c94782a95785eab82dbce7fe3
Rob <5183487+Rexeh@users.noreply.github.com>
2024-01-20 22:56:16 +0000

    remove broken tests in prep for rewrite

--- tests/star_citizen/test_sc_parser.py
+++ tests/star_citizen/test_sc_parser.py
@@ -6,106 +6,35 @@ import joystick_diagrams.plugins.star_citizen_plugin.star_citizen as sc
 class TestSCParserCases(unittest.TestCase):
     def setUp(self):
         self.file = sc.StarCitizen("./tests/data/star_citizen/layout_all_exported_valid.xml")
-        self.file.devices = {
-            "js1": {"guid": "0000000", "name": "Joystick"},
-            "kb1": {"guid": "1111111", "name": "Keyboard"},
-        }
-
-    def test_device_id_keyboard(self):
-        check = self.file.device_id("keyboard", 1)
-        self.assertEqual(check, "kb1")
-
-        check = self.file.device_id("keyboard", 2)
-        self.assertEqual(check, "kb2")
-
-    def test_device_id_joystick(self):
-        check = self.file.device_id("joystick", 1)
-        self.assertEqual(check, "js1")
-
-        check = self.file.device_id("joystick", 2)
-        self.assertEqual(check, "js2")
-
-    def test_device_id_invalid(self):
-        check = self.file.device_id("potato", 1)
-        self.assertEqual(check, "mo1")
-
-        check = self.file.device_id("potato", 2)
-        self.assertEqual(check, "mo2")
 
     def test_bind_parse_button(self):
-        data = self.file.parse_map("js1_button1")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "BUTTON_1"))
-
-        data = self.file.parse_map("js1_button22")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "BUTTON_22"))
-
-        data = self.file.parse_map("js1_button999")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "BUTTON_999"))
+        # ("js1_button1")
+        # ("js1_button22")
+        # ("js1_button999")
+        pass
 
     def test_bind_parse_blank(self):
-        data = self.file.parse_map("js1_")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, None))
+        # ("js1_")
+        pass
 
     def test_bind_parse_hat(self):
-        data = self.file.parse_map("js1_hat1_up")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "POV_1_U"))
-
-        data = self.file.parse_map("js1_hat1_right")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "POV_1_R"))
-
-        data = self.file.parse_map("js1_hat1_down")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "POV_1_D"))
-
-        data = self.file.parse_map("js1_hat1_left")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "POV_1_L"))
+        # "js1_hat1_up")
+        # ("js1_hat1_right")
+        # "js1_hat1_down")
+        # "js1_hat1_left")
+        pass
 
     def test_bind_parse_axis(self):
-        data = self.file.parse_map("js1_rotz")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "AXIS_Rz"))
-
-        data = self.file.parse_map("js1_x")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "AXIS_x"))
-
-        data = self.file.parse_map("js1_slider1")
-        self.assertEqual(data, ({"guid": "0000000", "name": "Joystick"}, "AXIS_SLIDER_1"))
+        # "js1_rotz")
+        # "js1_x")
+        # "js1_slider1")
+        pass
 
     def test_bind_no_device(self):
-        data = self.file.parse_map("abc_hat1_up")
-        self.assertEqual(data, (None, None))
+        pass
 
     def test_parser(self):
-        expected = {
-            "VKB-Sim Gladiator NXT R": {
-                "Default": {
-                    "Axis": "",
-                    "Buttons": {
-                        "BUTTON_9": "Capitalisetest",
-                        "BUTTON_10": "Fire 1",
-                        "BUTTON_1": "Close all doors",
-                        "BUTTON_2": "Open all doors",
-                        "BUTTON_3": "Unlock doors",
-                        "BUTTON_4": "Look behind",
-                    },
-                    "Inherit": False,
-                }
-            },
-            "VKB-Sim Gladiator NXT L": {
-                "Default": {
-                    "Axis": "",
-                    "Buttons": {
-                        "BUTTON_1": "View cycle fwd",
-                        "BUTTON_2": "View zoom abs",
-                        "AXIS_Ry": "View pitch",
-                        "AXIS_Rx": "View yaw",
-                        "BUTTON_22": "Cruise control",
-                    },
-                    "Inherit": False,
-                }
-            },
-        }
-
-        data = self.file.parse()
-        self.assertEqual(expected, data)
+        pass
 
 
 if __name__ == "__main__":
