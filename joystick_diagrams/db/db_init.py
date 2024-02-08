781f0150a17e343231f4770c8a0ba46ac879c310
Rob <5183487+Rexeh@users.noreply.github.com>
2024-02-08 08:00:57 +0000

    Fix missing data directory creation

--- joystick_diagrams/db/db_init.py
+++ joystick_diagrams/db/db_init.py
@@ -5,6 +5,7 @@ Author: Robert Cox
 
 import logging
 
+from joystick_diagrams import utils
 from joystick_diagrams.db import db_device_management, db_plugin_data, db_value_store
 
 _logger = logging.getLogger(__name__)
@@ -12,6 +13,8 @@ _logger = logging.getLogger(__name__)
 
 def init() -> None:
     _logger.info("Initialising datastores")
+
+    utils.create_directory("data")
     db_device_management.create_new_db_if_not_exist()
     db_value_store.create_new_db_if_not_exist()
     db_plugin_data.create_new_db_if_not_exist()
